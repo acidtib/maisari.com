39830aa052151e8f90ef890262e3729f6b476932
arubinofaux <arubinofaux@gmail.com>
2020-02-23 18:22:43 -0500

    wip

--- routes/projects.js
+++ routes/projects.js
@@ -6,4 +6,16 @@ router.get('/hodl-wallet', function (req, res, next) {
   res.render('project/hodl_wallet', { title: 'HODL WALLET' });
 });
 
+router.get('/bitstop-atm', function (req, res, next) {
+  res.render('project/bitstop_atm', { title: 'Bitstop ATM' });
+});
+
+router.get('/bitstop-co', function (req, res, next) {
+  res.render('project/bitstop_co', { title: 'Bitstop Co' });
+});
+
+router.get('/shoot-my-travel', function (req, res, next) {
+  res.render('project/shoot_my_travel', { title: 'Shoot My Travel' });
+});
+
 module.exports = router;
