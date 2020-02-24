8bd175e268eba5bc64bc05e5ac9fda8e757dcaa8
arubinofaux <arubinofaux@gmail.com>
2020-02-24 16:23:17 -0500

    exclude more

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const axios = require('axios');
 
 router.get('/dribbble', function (req, res, next) {
-  axios.get('https://api.dribbble.com/v2/user/shots', { 
+  axios.get('https://api.dribbble.com/v2/user/shots?per_page=100', { 
     headers: { Authorization: process.env.DRIBBBLE_TOKEN } 
   }).then(response => {
 
@@ -15,6 +15,32 @@ router.get('/dribbble', function (req, res, next) {
         case 7146866:
         case 7146862:
         case 7146046:
+        case 7146036:
+        case 7146033:
+        case 4444531:
+        case 4440391:
+        case 4436231:
+        case 4431103:
+        case 4430929:
+        case 4394011:
+        case 3072103:
+        case 2965545:
+        case 2958610:
+        case 2956243:
+        case 2956165:
+        case 2954928:
+        case 2954848:
+        case 2581574:
+        case 2575839:
+        case 2575818:
+        case 2575805:
+        case 2572205:
+        case 2572195:
+        case 2572193:
+        case 2571644:
+        case 2571640:
+        case 2571635:
+
           return ;
           break;
         default:
