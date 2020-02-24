8392fafda189f4be66d8ba612accd68e5ef3de64
arubinofaux <arubinofaux@gmail.com>
2020-02-24 17:47:52 -0500

    send out contact form

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var nodeMailer = require('nodemailer');
 
 /* GET contact page. */
 router.get('/', function (req, res, next) {
@@ -7,8 +8,31 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-  console.log(req.body)
-  res.json({ status: 'chill' })
+  let transporter = nodeMailer.createTransport({
+    host: 'smtp.zoho.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAILER_EMAIL,
+      pass: process.env.MAILER_PASSWORD
+    }
+  });
+
+  let mailOptions = {
+    from: process.env.MAILER_EMAIL,
+    to: process.env.MAILER_EMAIL,
+    subject: "New Message From Website",
+    html: "<b>name: " + req.body.senderName + "</b> <br> <b>email: " + req.body.senderEmail + "</b> <br><b>message: " + req.body.message + "</b>"
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: error });
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+    res.status(200).json({ status: 'chill' });
+  });
 });
 
 module.exports = router;
